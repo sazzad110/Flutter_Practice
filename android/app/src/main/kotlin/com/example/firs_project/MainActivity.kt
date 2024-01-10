2a4870840f8e0abab3fb63a78f9d9b55f6f5c19a
sazzad110 <sazzadhasan610@gmail.com>
2024-01-10 08:18:42 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/firs_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firs_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+
+  return runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(title: Text("hello flutter developer"),),
+      body: Center(child: Column(children: [
+        Text(
+          "this is",
+          textAlign: TextAlign.left,
+        ),
+      ],)),
+    )
+  ));
+}
