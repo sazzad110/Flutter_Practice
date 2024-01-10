8ce1eab0adf96242817dda14c768a689a4b489a4
sazzad110 <sazzadhasan610@gmail.com>
2024-01-10 09:08:33 +0600

    if we return placeholder we can see black screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,59 +14,22 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home_Screen(),    // Calling Home_Screen
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+// defining our first app page name Home_Screen
+class Home_Screen extends StatefulWidget {
+  const Home_Screen({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<Home_Screen> createState() => _Home_ScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
+class _Home_ScreenState extends State<Home_Screen> {
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return const Placeholder();           // eta return korle black screen asbe.
   }
 }
