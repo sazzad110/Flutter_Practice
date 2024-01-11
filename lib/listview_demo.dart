d34ce3a5ff08552948ac6183fb886e6566fc50b7
sazzad110 <sazzadhasan610@gmail.com>
2024-01-11 11:24:25 +0600

    optimize code

--- lib/listview_demo.dart
+++ lib/listview_demo.dart
@@ -12,78 +12,47 @@ class _ListViewDemoState extends State<ListViewDemo> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          child: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.greenAccent,
-              child: ListTile(
-                title: Text("sazzad hasan"),
-                subtitle: Text("0388394"),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.greenAccent,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.greenAccent,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.greenAccent,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.greenAccent,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.greenAccent,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.greenAccent,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.greenAccent,
-            ),
-          ),
-        ],
-      )),
+          child: ListView(children: [
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+        ListData(),
+      ])),
+    );
+  }
+}
+
+class ListData extends StatelessWidget {
+  const ListData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Container(
+        height: 100,
+        width: double.infinity,
+        color: Colors.greenAccent,
+        child: ListTile(
+          title: Text("sazzad hasan"),
+          subtitle: Text("0388394"),
+        ),
+      ),
     );
   }
 }
