17b7a3add94161dc4d618eb9ff70b924f11329e8
sazzad110 <sazzadhasan610@gmail.com>
2024-01-16 12:01:02 +0600

    tween animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,86 @@
-import 'package:firs_project/animated_crossfade_page.dart';
-import 'package:firs_project/contact_list.dart';
-import 'package:firs_project/page_view_demo.dart';
-import 'package:firs_project/stack_demo.dart';
 import 'package:flutter/material.dart';
-
-import 'listview_demo.dart';
-import 'modal_bottom_sheet.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: AnimatedCrossfadePage(), // Calling Home_Screen
+      home: MyAnimationPage(),
     );
+  }
+}
+
+class MyAnimationPage extends StatefulWidget {
+  @override
+  _MyAnimationPageState createState() => _MyAnimationPageState();
+}
+
+class _MyAnimationPageState extends State<MyAnimationPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize AnimationController
+    _controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    // Initialize Tween animation for scaling (from 0 to 1)
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Button Animation'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Animated text
+            ScaleTransition(
+              scale: _animation,
+              child: Text(
+                'Hello, World!',
+                style: TextStyle(
+                  fontSize: 1,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            // Button to trigger the animation
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Reset the animation to its initial state before starting
+                _controller.reset();
+
+                // Start the animation
+                _controller.forward();
+              },
+              child: Text('Animate'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed
+    _controller.dispose();
+    super.dispose();
   }
 }
